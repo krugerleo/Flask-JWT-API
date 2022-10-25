b5e9779e2e715f7678d0524a6fe1f4bd20dbbefa
Leonardo Kruger <kruger.leob@gmail.com>
2022-10-24 21:03:40 -0300

    add paginate and update bookmark

--- src/bookmarks.py
+++ src/bookmarks.py
@@ -40,12 +40,17 @@ def post_bookmark():
 @bookmarks.get('/')
 @jwt_required()
 def get_bookmarks():
+
+    page = request.args.get('page',1, type=int)
+    per_page = request.args.get('per_page',5,type=int)
+
     current_user = get_jwt_identity()
-    bookmarks = Bookmark.query.filter_by(user_id = current_user)
+    bookmarks = Bookmark.query.filter_by(user_id = current_user).paginate(page=page, per_page=per_page)
+    
 
     data = []
 
-    for bookmark in bookmarks:
+    for bookmark in bookmarks.items:
         data.append({
             'ID':bookmark.id,
             'url': bookmark.url,
@@ -56,7 +61,16 @@ def get_bookmarks():
             'updated_at': bookmark.updated_at
         })
     
-    return jsonify({'data':data}),HTTP_200_OK
+    meta ={
+        "page": bookmarks.page,
+        "pages": bookmarks.pages,
+        'total_count': bookmarks.total,
+        'prev':bookmarks.prev_num,
+        'next': bookmarks.next_num,
+        'has_next': bookmarks.has_next,
+        'has_prev': bookmarks.has_prev
+    }
+    return jsonify({'data':data, 'meta':meta}),HTTP_200_OK
 
 @bookmarks.get('/<int:id>')
 @jwt_required()
@@ -70,6 +84,42 @@ def get_bookmark(id):
             'message':'Item not found'
         }), HTTP_404_NOT_FOUND
 
+    return jsonify({
+        'ID':bookmark.id,
+        'url': bookmark.url,
+        'short_url': bookmark.short_url,
+        'visits': bookmark.visits,
+        'body': bookmark.body,
+        'created_at': bookmark.created_at,
+        'updated_at': bookmark.updated_at
+    }), HTTP_200_OK
+
+@bookmarks.patch('/<int:id>')
+@bookmarks.put('/<int:id>')
+@jwt_required()
+def update_bookmark(id):
+    current_user = get_jwt_identity()
+
+    bookmark = Bookmark.query.filter_by(user_id=current_user, id=id).first()
+
+    if not bookmark:
+        return jsonify({
+            'message':'Item not found'
+        }), HTTP_404_NOT_FOUND
+    
+    body = request.get_json().get('body', '')
+    url = request.get_json().get('url', '')
+
+    if not validators.url(url):
+        return jsonify({
+            'error': 'Enter a valid url'
+        }), HTTP_400_BAD_REQUEST
+
+    bookmark.url = url
+    bookmark.body = body
+
+    db.session.commit()
+    
     return jsonify({
         'ID':bookmark.id,
         'url': bookmark.url,
